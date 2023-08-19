e7db4248d5484e22cccd4a2f231903de65e19544
rhymald <rhymald@gmail.com>
2023-08-19 12:37:48 +0200

    + Move+path copy-paste

--- balance/functions/time.go
+++ balance/functions/time.go
@@ -4,8 +4,14 @@ import (
 	"time"
 )
 
+const (
+	TAxisStep = 400 //ms for grid
+	TRange = 80 //steps per bucket, must be >= Retro
+)
+
 var StartEpoch = int(time.Now().UnixNano())
 
+func TAxis() int { return (Epoch()/TAxisStep)%TRange }
 func Epoch() int { return EpochNS()/1000000 }
 func EpochNS() int { return int(time.Now().UnixNano())-StartEpoch }
-// func Wait(ms float64) { time.Sleep( time.Millisecond * time.Duration( ms )) }
+func Wait(ms float64) { time.Sleep( time.Millisecond * time.Duration( ms )) }
--- play/state.go
+++ play/state.go
@@ -2,15 +2,24 @@ package play
 
 import (
 	"rhymald/mag-eta/balance/primitives"
+	"sync"
 )
 
+func CleanTrace() *Tracing { return &Tracing{ Trxy: make(map[int][3]int) }}
+type Tracing struct {
+	Trxy map[int][3]int
+	sync.Mutex
+}
+
 type State struct {
+	Trace [3]*Tracing
 	// Effects
 	Write struct {
-		Body primitives.Stream // Body
-		HP primitives.Health// Health 
+		Body primitives.Stream
+		HP primitives.Health
 		// Actions 
 	}
 	Current *Character
 	Later Character
 }
+
