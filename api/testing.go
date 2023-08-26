ea4fa148d50689c2bcad345d3fd975188c0e0ce7
rhymald <rhymald@gmail.com>
2023-08-26 09:21:20 +0200

    + Filter actual pos for collide (heavy)

--- api/testing.go
+++ api/testing.go
@@ -2,7 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	// "rhymald/mag-eta/balance/functions"
+	"rhymald/mag-eta/balance/functions"
+	"fmt"
 )
 
 type Testing_Response struct {
@@ -19,7 +20,18 @@ func testWorld(c *gin.Context) {
 	buffer := writer.Get_Square(0, 0, 2000)
 	old := reader.Get_Square(0, 0, 2000)
 	for id, row := range old { if _, ok := buffer[id] ; !ok { 
+		row[2] += -functions.TRange
 		buffer[id] = row
+	}}
+	list := []string{}
+	states := (*theWorld).ByID.GetAll()
+	for t:=functions.TRange-1 ; t>=-functions.TRange ; t-- { for id, row := range buffer {
+		player, ok := states[id]
+		path := player.Path()
+		actual := row[0] == path[1][0] && row[1] == path[1][1] && ok
+		if row[2] == t && actual {
+			list = append(list, fmt.Sprintf("id = %s, x = %6d, y = %6d, t = %3d, old = %6dms", id, row[0], row[1], row[2], row[3]))
+		}
 	}}
 	// buffer.PosR[0], buffer.PosR[1] = reader.Get_CentralPos()
 	// buffer.PosW[0], buffer.PosW[1] = writer.Get_CentralPos()
@@ -31,5 +43,5 @@ func testWorld(c *gin.Context) {
 	// buffer.Xw = writer.X.Get(targetX, targetAOE, targetT)
 	// buffer.Yw = writer.Y.Get(targetY, targetAOE, targetT)
 	// writer.Unlock()
-	c.IndentedJSON(200, buffer)
+	c.IndentedJSON(200, list)
 }
